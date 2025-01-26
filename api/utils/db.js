a55e01e9ce1cb1b5c143e5819e8e536e5ceffd2e
DummyKim <actiont1213@naver.com>
2025-01-26 17:09:09 +0900

    Add files via upload

--- api/utils/db.js
+++ api/utils/db.js
@@ -3,9 +3,6 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     const uri = process.env.MONGO_URI;
 
-    // 디버깅용 로그 추가
-    console.log('MongoDB URI:', uri); 
-
     if (!uri) {
         throw new Error('MONGO_URI 환경 변수가 설정되지 않았습니다.');
     }
