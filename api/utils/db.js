076f858c0302709f30e6c2aaf38a8ff76668d2d6
DummyKim <actiont1213@naver.com>
2025-01-26 17:19:12 +0900

    Add files via upload

--- api/utils/db.js
+++ api/utils/db.js
@@ -3,18 +3,12 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     const uri = process.env.MONGO_URI;
 
-    // 디버깅용 로그 추가
-    console.log('MongoDB URI:', uri); 
-
     if (!uri) {
         throw new Error('MONGO_URI 환경 변수가 설정되지 않았습니다.');
     }
 
     try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(uri);
         console.log('MongoDB 연결 성공');
     } catch (error) {
         console.error('MongoDB 연결 실패:', error.message);
@@ -23,5 +17,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
-//디플로이용 수정사항//
